99a8838bdb413ef23e6d5e37384049f09072173c
L480 <mail@nico-schiering.de>
2024-04-06 21:52:28 +0200

    Prettifying

--- workers/index.js
+++ workers/index.js
@@ -2,10 +2,11 @@ export default {
     async scheduled(event, env, ctx) {
         await this.fetch(event, env, ctx); // To keep the access token fresh when no one is accessing the page.
     },
+
     async fetch(request, env, ctx) {
         const tokenEndpoint = 'https://login.tonies.com/auth/realms/tonies/protocol/openid-connect/token';
         const graphqlEndpoint = 'https://api.prod.tcs.toys/v2/graphql';
-        const tokenBody = 'grant_type=refresh_token&refresh_token=' + await env.kv.get('REFRESH_TOKEN') + '&client_id=my-tonies';
+        const tokenBody = `grant_type=refresh_token&refresh_token=${await env.kv.get('REFRESH_TOKEN')}&client_id=my-tonies`;
         const graphqlBody = '{ "query": "query Tonies { households { contentTonies { title series { name } imageUrl } creativeTonies { name imageUrl }}}", "operationName": "Tonies" }';
         const useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
         const cacheTtl = 3600;
@@ -45,7 +46,7 @@ export default {
             body: graphqlBody,
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer ' + tokenResponse.access_token,
+                'Authorization': `Bearer ${tokenResponse.access_token}`,
                 'Content-Type': 'application/json',
                 'User-Agent': useragent
             },
@@ -71,7 +72,7 @@ export default {
         // JSON response with cache and CORS
         return Response.json(allTonies, {
             headers: {
-                'Cache-Control': 'max-age=' + cacheTtl,
+                'Cache-Control': `max-age=${cacheTtl}`,
                 'Access-Control-Allow-Origin': env.CORS_ORIGINS,
                 'Access-Control-Allow-Methods': 'GET',
             }
